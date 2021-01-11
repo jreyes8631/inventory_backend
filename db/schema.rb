c574c96086fd56887d6269c7882328038b0eb533
jreyes8631 <jreyes8631@gmail.com>
2021-01-11 08:09:11 -0500

    refactor jti

--- db/schema.rb
+++ db/schema.rb
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 2021_01_09_211906) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "jti"
+    t.string "jti", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["jti"], name: "index_users_on_jti"
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
