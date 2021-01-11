61676f1425d38b86210b81ab684346898ff97b30
jreyes8631 <jreyes8631@gmail.com>
2021-01-11 08:07:18 -0500

    refactor devise registerable, rememberable, ect

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,6 @@ class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
-         :jwt_authenticatable, jwt_revocation_strategy: self
-
-    def jwt_payload
-      super.merge('foo' => 'bar')
-    end
+  devise :database_authenticatable, :registerable, :rememberable, :validatable,
+  :recoverable, :jwt_authenticatable, jwt_revocation_strategy: self
 end
