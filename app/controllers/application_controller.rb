a38f26e64ec33b435a634b4ddb00893c9361f163
jreyes8631 <jreyes8631@gmail.com>
2021-01-12 07:14:34 -0500

    add before action authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+    before_action :authenticate_user!
+
+    def current_user
+        User.first
+    end
 end
